6094ebe838ca30ba68bbafd968807865c719cb00
Daulet <baimurza.daulet@gmail.com>
2024-10-09 18:48:57 +0500

    Edit router.go, sql, base.html

--- internal/app/router.go
+++ internal/app/router.go
@@ -1,12 +1,72 @@
 package app
 
 import (
+	"database/sql"
+	"os/user"
+
 	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/v5/middleware"
+	"github.com/golang-jwt/jwt/v5"
+	"github.com/sirupsen/logrus"
+	"golang.org/x/tools/go/cfg"
+	"shanraq.org/config"
 	"shanraq.org/internal/handlers"
+	"shanraq.org/internal/repositories"
+	"shanraq.org/internal/services"
 )
 
-func SetupRouter() *chi.Mux {
+func SetupRouter(cfg *config.Config, logger *logrus.Logger) *chi.Mux {
 	r := chi.NewRouter()
+
+	// Middlewares
+	r.Use(middleware.RequestID)
+	r.Use(middleware.RealIP)
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
+
+	// Connection to database
+	db, err := sql.Open("postgres", cfg.Database.DSN())
+	if err != nil {
+		logger.Fatalf("Database connection error: %v", err)
+	}
+
+	// Repositories
+	userRepo := repositories.NewUserRepository(db)
+	// Other repositories...
+
+	// Services
+	iwtService := services.JWTService{SecretKey: cfg.JWT.SekretKey}
+	authService := services.AuthService{UserRepo: userRepo, TokenService: services.JWTService}
+	// Other services...
+
+	// Heandlers
+	authHeandler := handlers.NewAuthHeandler(authService)
+	// Other heandlers...
+
+	// Routers
+	r.Post("/register", authHeandler.Register)
+	r.Post("/login", authHeandler.login)
+
+	r.Group(func(r chi.Router) {
+		r.Use(AuthMiddleware(jwtService))
+
+		r.Route("/articles", func(r chi.Router) {
+			r.Get("/", handlers.GetArticles)
+			r.Post("/", handlers.CreateArticle)
+			r.Put("/{id}", handlers.UpdateArticle)
+			r.Delete("/{id}", handlers.DeleteArticle)
+		})
+
+		// Admin dashboard routers
+		r.Group(func(r chi.Router) {
+			r.Use(AdminOnlyMiddleware)
+			r.Route("/admin", func(r chi.Router) {
+				// Heandler admin dashboard
+			})
+		})
+	})
+
+	return r
 
 	r.Router("/articles", func(r chi.Router) {
 		r.Get("/", handlers.GetArticles)
